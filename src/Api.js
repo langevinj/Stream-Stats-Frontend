e0c31f2a7796938cf6e1bb435a3e730e008f01ac
Jeremy Langevin <langevinj832@gmail.com>
2021-01-11 13:13:07 -0500

    Addition of variable base URL

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_API_URL = "http://localhost:5000"
+const BASE_API_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000"
 
 /**
  * class containing methods for reaching out to API endpoint

2a83251ec4d8cf3b51069516fc9c12fc627f07eb
Jeremy Langevin <langevinj832@gmail.com>
2021-01-11 13:02:27 -0500

    Home component skeleton added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }

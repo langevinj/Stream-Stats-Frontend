3de49ba0b41fb2ba7a56767ba3023fad9c718d71
Jeremy Langevin <langevinj832@gmail.com>
2021-01-11 13:11:18 -0500

    Skeleton of API component created

---
+++ src/Api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const BASE_API_URL = "http://localhost:5000"
+
+/**
+ * class containing methods for reaching out to API endpoint
+ */
+class StreamingApi {
+
+}
+
+export default StreamingApi;

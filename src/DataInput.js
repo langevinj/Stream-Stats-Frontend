0ec2ba948a20ea335415e7968c50e7857e60e822
Jeremy Langevin <langevinj832@gmail.com>
2021-01-18 14:17:39 -0500

    Code cleanup

--- src/DataInput.js
+++ src/DataInput.js
@@ -25,24 +25,20 @@ function DataInput(){
             [name]: value
         }));
     }
+    
     let responses = [];
+
     const handleSubmit = async (evt) => {
         evt.preventDefault();
 
         const starterPromise = Promise.resolve(null);
+        //run through each submissing in sequence as to not overload db at once
         const log = result => console.log(result);
         await [handleBandcamp(), handleSpotifyAll(), handleSpotifyMonth(), handleSpotifyCredentials(), handleDistrokid()].reduce(
             (p, spec) => p.then(() => spec.then(log)),
             starterPromise
         )
         
-        // await Promise.all([
-        //     handleBandcamp(),
-        //     handleSpotifyAll(),
-        //     handleSpotifyMonth(),
-        //     handleSpotifyCredentials(),
-        //     handleDistrokid()
-        // ]);
         return responses;
     }
 
@@ -124,118 +120,6 @@ function DataInput(){
         }
     }
 
-
-
-    // async function handleSubmit(evt) {
-        
-
-    //     //list of responses from adding data to the backend
-    //     let responses = []
-        
-    //     //if distrokid information is passed, process it
-        
-    //     await handleDistrokid();
-    //     async function handleDistrokid(){
-    //         if (formData.distrokid) {
-    //             //format the pasted distrokid page
-    //             let data = { page: formData.distrokid, username: currUser.username };
-
-    //             try {
-    //                 let res = await StreamingApi.distrokidImport(data);
-    //                 //add response to response list
-    //                 responses.push(res);
-    //             } catch (errors) {
-    //                 return setFormData(f => ({ ...f, errors }));
-    //             }
-    //         }
-    //         await handleBandcamp();
-    //     }
-
-    //     async function handleBandcamp(){
-    //         //if bandcamp data is passed, process it
-    //         if (formData.bandcamp) {
-    //             //format the pasted bandcamp page
-    //             let data = { page: formData.bandcamp, username: currUser.username };
-
-    //             try {
-    //                 let res = await StreamingApi.bandcampImport(data);
-    //                 //add response to response list
-    //                 responses.push(res);
-    //             } catch (errors) {
-    //                 return setFormData(f => ({ ...f, errors }));
-    //             }
-    //         }
-    //         await handleSpotifyCredentials();
-    //     }
-
-    //     async function handleSpotifyCredentials(){
-    //         //if spotify credentials are passed, process them
-    //         if (formData.spotifyEmail && formData.spotifyPwd) {
-    //             //send pre-hashed password and email for spotify credentials to be saved
-    //             let data = { email: formData.spotifyEmail, password: formData.spotifyPwd, username: currUser.username }
-
-    //             try {
-    //                 // let res = await StreamingApi.saveUserSpotifyCredentials(data);
-    //                 let res = await StreamingApi.gatherSpotifyData(data)
-    //                 //add response to response list
-    //                 responses.push(res);
-    //             } catch (errors) {
-    //                 return setFormData(f => ({ ...f, errors }));
-    //             }
-    //         }
-    //         await handleSpotifyMonth();
-    //     }
-
-    //     async function handleSpotifyMonth(){
-    //         //if spotify page for a month sort is passed, process it
-    //         if (formData.spotifyRawMonth) {
-    //             let data = { page: formData.spotifyRawMonth, username: currUser.username };
-
-    //             try {
-    //                 let res = await StreamingApi.spotifyMonthImport(data);
-    //                 //add response to response list
-    //                 responses.push(res);
-    //             } catch (errors) {
-    //                 return setFormData(f => ({ ...f, errors }));
-    //             }
-    //         }
-    //         await handleSpotifyAll();
-    //     }
-
-    //     async function handleSpotifyAll(){
-    //         //if spotify page for all time is passed, process it
-    //         if (formData.spotifyRawAll) {
-    //             let data = { page: formData.spotifyRawAll, username: currUser.username };
-
-    //             try {
-    //                 let res = await StreamingApi.spotifyAlltimeImport(data);
-    //                 //add response to response list
-    //                 responses.push(res);
-    //             } catch (errors) {
-    //                 return setFormData(f => ({ ...f, errors }));
-    //             }
-    //         }
-    //     }
-
-        
-
-        
-
-        
-
-        
-    //     return responses;
-    //     //return to homepage, might want to change this depending on where you want to send user
-    //     // history.push("/");
-    // }
-
-    //quick trial function for gathering spotify data via scrape
-    // async function gatherSpotifyData(evt){
-    //     evt.preventDefault();
-    //     let res = await StreamingApi.gatherSpotifyData({ username: currUser.username });
-    //     console.log(res)
-    // }
-    
     // Copy the entire page(MAC: Cmd + A / WIN: Ctrl + A) then paste here:
     return (
         <div className="container">

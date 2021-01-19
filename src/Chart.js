391419ee3bc140f3bea57d846b1594a981812534
Jeremy Langevin <langevinj832@gmail.com>
2021-01-19 09:06:00 -0500

    Added basic Chart component

---
+++ src/Chart.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect, useContext} from 'react'
+import StreamingApi from './Api'
+import UserContext from './UserContext'
+
+function Chart(){
+    const { currUser } = useContext(UserContext);
+    const [bandcampData, setBandcampData] = useState(null)
+
+    //get bandcamp data for user upon loading page
+    useEffect(() => {
+        async function getBandcampData() {
+            try {
+                let data = await StreamingApi.getUserBandcampData({ range: "alltime"}, currUser.username);
+                console.log(data);
+                setBandcampData(data);
+            } catch (err) {
+                throw err;
+            }
+        }
+        getBandcampData();
+    }, []);
+
+
+}
+
+export default Chart;

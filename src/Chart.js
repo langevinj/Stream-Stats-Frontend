56595a43f1bd56797f2daf4fc2b02c493ec8007d
Jeremy Langevin <langevinj832@gmail.com>
2021-01-21 07:57:36 -0500

    Creating master list of all songs

--- src/Chart.js
+++ src/Chart.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect, useContext} from 'react'
 import StreamingApi from './Api'
 import UserContext from './UserContext'
 import { v4 as uuid} from 'uuid'
-import { XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, VerticalBarSeries, VerticalBarSeriesCanvas, DiscreteColorLegend, Crosshair, Hint } from 'react-vis';
+import { XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, VerticalBarSeries, VerticalBarSeriesCanvas, DiscreteColorLegend, Hint } from 'react-vis';
 import { colorsMap } from './colors.js'
 import './style.css';
 
@@ -11,25 +11,62 @@ function ChartData(){
     const [bandcampData, setBandcampData] = useState([]);
     const [distrokidData, setDistrokidData] = useState([]);
     const [spotifyData, setSpotifyData] = useState([]);
+    const [chartRange, setChartRange] = useState("alltime");
+    const graphItems = []
 
-    //get bandcamp data for user upon loading page
+    //get streaming data for user upon loading page
     useEffect(() => {
-        async function getBandcampData() {
+        async function getUserData() {
             try {
-                let bdata = await StreamingApi.getUserBandcampData({ range: "alltime" }, currUser.username);
+                let bdata = await StreamingApi.getUserBandcampData({ range: chartRange }, currUser.username);
                 setBandcampData(bdata);
-                let ddata = await StreamingApi.getUserDistrokidData({ range: "alltime" }, currUser.username);
+                let ddata = await StreamingApi.getUserDistrokidData({ range: chartRange }, currUser.username);
                 setDistrokidData(ddata);
-                let sdata = await StreamingApi.getUserSpotifyData({ range: "alltime" }, currUser.username);
+                let sdata = await StreamingApi.getUserSpotifyData({ range: chartRange }, currUser.username);
                 setSpotifyData(sdata);
             } catch (err) {
                 throw err;
             }
         }
-        getBandcampData();
+        getUserData();
     }, [currUser.username]);
+    console.log(spotifyData)
+    //create an array of all songs released by the artist/musician
+    const allSongs = []
+    function listAllSongs(bandcampData, spotifyData, distrokidData){
+        if(bandcampData){
+            for(let d of bandcampData){
+                if(!allSongs.includes(d.title)) allSongs.push(d.title);
+            }
+        }
 
-    const graphItems = []
+        if(spotifyData){
+            for(let d of spotifyData) {
+                if(!allSongs.includes(d.title)) allSongs.push(d.title);
+            }
+        }
+
+        if(distrokidData){
+            for(let d of distrokidData){
+                if(!allSongs.includes(d.title)) allSongs.push(d.title)
+            }
+        }
+
+    }
+    listAllSongs(bandcampData, spotifyData, distrokidData)
+    console.log(allSongs)
+    //go through the bandcamp data and format it correctly
+    const bandcampPlaysData = [];
+
+    for (let d of bandcampData) {
+        bandcampPlaysData.push({ x: d.title, y: d.plays });
+    }
+
+    //add bandcamp data to the overall graphItems data list
+    graphItems.push(bandcampPlaysData);
+
+    //create an array of all song titles
+    // const allSongs = bandcampPlaysData.map(b => b.x)
 
     //make an array of all the songs listed on spotify
     const spotifySongs = []
@@ -47,17 +84,7 @@ function ChartData(){
         }
     }
 
-    //go through the bandcamp data and format it correctly
-    const bandcampPlaysData = [];
-
-    for(let d of bandcampData){
-        bandcampPlaysData.push({x: d.title, y: d.plays});
-    }
-
-    graphItems.push(bandcampPlaysData);
-
-    //create an array of all song titles
-    const allSongs = bandcampPlaysData.map(b => b.x)
+    
 
     const formattedSpotifyData = [];
     //format the spotify data for the table

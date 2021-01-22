dafd3de6d2b269259b940efeb8edd53a9330c217
Jeremy Langevin <langevinj832@gmail.com>
2021-01-22 10:12:58 -0500

    Adjusted navbar style

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,7 +11,7 @@ function NavBar({ logOut }) {
  
     //navbar view if a user is logged in
     const loggedInView = (
-        <nav className="navbar navbar-expand-sm navbar-light bg-white">
+        <nav className="navbar navbar-expand-sm navbar-light">
             <NavLink exact to="/" className="navbar-brand">Stream Stats</NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target='#navbarNavDropdown' aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>

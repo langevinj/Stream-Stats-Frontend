4bae478c15a6c8481e03dace3448bdcd52a3a4c1
Jeremy Langevin <langevinj832@gmail.com>
2021-01-25 15:27:16 -0500

    App.test.js written

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,3 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 import React from 'react'
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom'

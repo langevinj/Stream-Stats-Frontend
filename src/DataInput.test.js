ca806db78712ff5bd70dd6753d3b931d09de4017
Jeremy Langevin <langevinj832@gmail.com>
2021-02-01 14:59:36 -0500

    Added handles toggles test

--- src/DataInput.test.js
+++ src/DataInput.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DataInput from './DataInput'
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom'
 import UserContext from './UserContext'
@@ -24,4 +24,21 @@ it('matches snapshot', function(){
     </MemoryRouter>);
 
     expect(asFragment).toMatchSnapshot();
+});
+
+it('handles toggles', function () {
+    const { getByText, queryByTestId } = render(<MemoryRouter>
+        <UserContext.Provider value={{ currUser, setCurrUser }}>
+            <DataInput />
+        </UserContext.Provider>
+    </MemoryRouter>);
+
+    expect(getByText("View how-to video")).toBeInTheDocument();
+    expect(getByText("I prefer to paste my spotify data")).toBeInTheDocument();
+
+    fireEvent.click(getByText("View how-to video"));
+    fireEvent.click(getByText("I prefer to paste my spotify data"));
+
+    expect(queryByTestId("toggleVideoOn")).not.toBeInTheDocument();
+    expect(queryByTestId("toggleVideoOff")).toBeInTheDocument();
 });

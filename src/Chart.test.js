9e6993c1d6bc0d11a1535b4a39eb7a58a8688d5b
Jeremy Langevin <langevinj832@gmail.com>
2021-02-01 11:40:54 -0500

    Creation of Chart.test.js

---
+++ src/Chart.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+import Chart from './Chart.js';
+

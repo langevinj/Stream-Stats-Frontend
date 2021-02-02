d14cfc75ff21cb988d4cc97d1b61a3efc44311f1
Jeremy Langevin <langevinj832@gmail.com>
2021-02-02 11:24:31 -0500

    Test written for toggling views

--- src/Chart.test.js
+++ src/Chart.test.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
 import Chart from './Chart.js';
@@ -32,4 +32,40 @@ it("matches the snapshot", function() {
     </MemoryRouter>);
 
     expect(asFragement).toMatchSnapshot();
+});
+
+it("shows a message indicating no data has been added", function() {
+    const { getByText } = render(<MemoryRouter>
+        <UserContext.Provider value={{ currUser, setCurrUser }}>
+            <Chart />
+        </UserContext.Provider>
+    </MemoryRouter>);
+
+    //Wait for render lag
+    setTimeout(() => {
+        expect(getByText("No data has been imported for All time yet. Wait a few seconds, or import data now.")).toBeInTheDocument();
+    }, 2000);
+});
+
+it("toggles between views", function() {
+    const { getByText, queryByTestId } = render(<MemoryRouter>
+        <UserContext.Provider value={{ currUser, setCurrUser }}>
+            <Chart />
+        </UserContext.Provider>
+    </MemoryRouter>);
+    
+    
+    let title = queryByTestId("chartTitle");
+    setTimeout(() => {
+        expect(title.innerText).toEqual('All time');
+    }, 1000);
+    
+    let toggleButtom = getByText("30-day");
+    fireEvent.click(toggleButtom);
+
+    title = queryByTestId("chartTitle");
+
+    setTimeout(() => {
+        expect(title.innerText).toEqual('30-day');
+    }, 1000);
 });

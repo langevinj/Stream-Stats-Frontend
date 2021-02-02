2ad392f2cf3b216e00e4030c69a033e5851a0916
Jeremy Langevin <langevinj832@gmail.com>
2021-02-02 10:26:31 -0500

    Snapshot and smoke tests written

--- src/Login.test.js
+++ src/Login.test.js
@@ -3,3 +3,19 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
 import Login from './Login.js';
+
+const mockSetToken = jest.fn(() => "");
+
+it("renders without crashing", function() {
+    render(<MemoryRouter>
+        <Login setToken={mockSetToken} />
+    </MemoryRouter>);
+});
+
+it("matches the snapshot", function () {
+    const {asFragment} = render(<MemoryRouter>
+        <Login setToken={mockSetToken} />
+    </MemoryRouter>);
+
+    expect(asFragment).toMatchSnapshot();
+});

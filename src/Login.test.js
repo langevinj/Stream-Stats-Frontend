cb76ec39219f18493cebd66b22207886a062492b
Jeremy Langevin <langevinj832@gmail.com>
2021-02-02 10:10:58 -0500

    Creation of Login.test.js file

---
+++ src/Login.test.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+import Login from './Login.js';

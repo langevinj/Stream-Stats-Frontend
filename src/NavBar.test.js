2e9524593aeeae81e805e4697a3f5023abed2655
Jeremy Langevin <langevinj832@gmail.com>
2021-02-02 10:33:05 -0500

    Creation of NavBar.test.js file

---
+++ src/NavBar.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import UserContext from './UserContext';
+import '@testing-library/jest-dom';
+import NavBar from './NavBar';
